0707e2d7847172aa353e85bd9723ab61b96dee82
DikarevAndrey <dias7707@gmail.com>
2017-12-20 03:51:58 +0300

    Added view for the place.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,7 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, except: %i[index categories]
+	before_action :authenticate_user!, except: %i[index categories show]
+  before_action :find_place, only: %i[show edit update destroy]
+
   def index
   end
 
@@ -7,28 +9,24 @@ class PlacesController < ApplicationController
     @categories = Category.all.map { |e| { id: e.id, name: e.name } }
   end
 
+  def show
+    redirect_to newPlace_path unless @place
+  end
+
   def new_place
   	@place = current_user.places.build
-    # @place = Place.new(user_id: current_user.id)
   end
 
   def create
     @place = current_user.places.build(place_params)
     @place.category_id = params[:category_id]
-    logger.debug @place.valid?
     if @place.save
       logger.debug 'Saved new place'
-      logger.debug @place.address
-      logger.debug @place.longitude
-      logger.debug @place.latitude
       flash[:notice] = "Place successfully created"
       redirect_to root_path
     else
       logger.debug 'Did not save new place'
-      logger.debug @place.address
-      logger.debug @place.longitude
-      logger.debug @place.latitude
-      flash.alert = "Incorrect address"
+      # flash.alert = "Incorrect address"
       render :new_place
     end
   end
@@ -37,7 +35,13 @@ class PlacesController < ApplicationController
     @categories = Category.all.map { |e| { id: e.id, name: e.name } }
   end
 
+  private
+
   def place_params
     params.require(:place).permit(:name, :description, :category_id, :address)
+  end
+
+  def find_place
+    @place = Place.find_by(id: params[:id])
   end
 end
